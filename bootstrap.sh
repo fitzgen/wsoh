0b0ca5c78f6a048c0d844ae0520d99b0e74f811a
Nick Fitzgerald <fitzgen@gmail.com>
2011-07-22 21:11:47 -0700

    Fixing bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
 virtualenv --no-site-packages -p `which python2.7` environment
 cd environment
 source bin/activate
@@ -7,13 +11,13 @@ pip install -r requirements.txt
 
 pycoweb deploy .
 
+rm -rf www
+rm -rf bots
 ln -s ../www www
 ln -s ../bots bots
 
 cp ../bin/run_server.py bin/run_server.py
 
 echo "# To start, run:"
-echo cd environment/
-echo source bin/activate
-echo run_server.py
+echo "cd environment/ && source bin/activate && run_server.py"
 echo "# And open browser to http://localhost:8080/pragmatico/www/index.html"
